6b5504ccc532192ee99cd27e6125c4af31c27492
rtkay123 <rodney.kanjala@sybrin.com>
2023-08-07 15:53:15 +0200

    chore: adjust codecov for apm spans/transactions

--- jest.config.ts
+++ jest.config.ts
@@ -54,7 +54,7 @@ const config: Config.InitialOptions = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 81,
+      branches: 68,
       functions: 95,
       lines: 95,
       statements: 95,
